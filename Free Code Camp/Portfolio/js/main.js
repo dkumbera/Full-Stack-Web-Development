d37cf9a864275f061cc35cfec884bbdb2ecd0162
Danny Kumbera <dannykumbera@gmail.com>
2019-05-15 16:50:06 -0700

    Updated the navbar to be responsive

--- Free Code Camp/Portfolio/js/main.js
+++ Free Code Camp/Portfolio/js/main.js
@@ -1,8 +1,8 @@
-function toggleDropdown(){
+/*function toggleDropdown(){
     let navbarToggle=document.getElementById("navbar-toggle");
     if(navbarToggle.className === 'topnav'){
         navbarToggle.className += ' responsive';
     } else{
         navbarToggle.className = 'topnav';
     }
-}
+}*/
